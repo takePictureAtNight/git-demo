05e83475778881d88c1376c9be7bf4d862761407
yy <3110311633@qq.com>
2023-03-17 10:16:33 +0800

    热修

--- src/main/java/com/yy/GitDemo.java
+++ src/main/java/com/yy/GitDemo.java
@@ -4,5 +4,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("测试git-demo");
         System.out.println("第二次测试...");
+        System.out.println("热修分支...");
     }
 }
