880536bb8ce6e6089718541934938f52c162ddd1
BlackCarp <97046123+takePictureAtNight@users.noreply.github.com>
2023-03-18 20:28:55 +0800

    失败
    失败

--- src/main/java/com/yy/GitDemo.java
+++ src/main/java/com/yy/GitDemo.java
@@ -6,6 +6,7 @@ public class GitDemo {
         System.out.println("第二次测试...");
         System.out.println("热修分支...");
         System.out.println("test冲突测试...");
-         System.out.println("rebase测试拉取...");
+        System.out.println("rebase测试拉取...");
+         System.out.println("失败");
     }
 }
