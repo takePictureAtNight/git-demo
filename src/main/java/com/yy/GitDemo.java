0b393a2af8bbfb8f9b4c335364ec27c676038e8b
BlackCarp <97046123+takePictureAtNight@users.noreply.github.com>
2023-03-19 16:27:47 +0800

    Update GitDemo.java
    1

--- src/main/java/com/yy/GitDemo.java
+++ src/main/java/com/yy/GitDemo.java
@@ -7,5 +7,7 @@ public class GitDemo {
         System.out.println("热修分支...");
         System.out.println("hot-fix冲突测试...");
         System.out.println("测试fetch...");
+         System.out.println("");
+        
     }
 }
